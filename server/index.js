267380df8b5331adf53b31f0a0ebaa7947afec64
rajharshiitian <rajharshiitian@gmail.com>
2024-08-25 18:33:56 +0530

    firt finalt

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import UserRouter from "./routes/User.js";
 import ProductRoutes from "./routes/Products.js";
-
+//require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
@@ -47,7 +47,7 @@ const connectDB = () => {
 const startServer = async () => {
   try {
     connectDB();
-    app.listen(8080, () => console.log("Server started on port 8080"));
+    app.listen(process.env.PORT || 8080, () => console.log("Server started on port 8080"));
   } catch (error) {
     console.log(error);
   }
